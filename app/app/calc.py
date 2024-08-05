34e744349392eb34010cb9c80508252d1c351ada
mubarak-adeshina-tfs <mubarak.adeshina@thermofisher.com>
2024-08-05 17:28:02 -0400

    first commit

--- app/app/calc.py
+++ app/app/calc.py
@@ -5,3 +5,7 @@ Add two number function
 
 def add(x, y):
     return x + y
+
+
+def subtract(x, y):
+    return x - y
--- app/app/test.py
+++ app/app/test.py
@@ -7,3 +7,8 @@ class CalcTestcase(SimpleTestCase):
         """" Add two number test """
         res = calc.add(2, 3)
         self.assertEqual(res, 5)
+
+    def test_subtract_numbers(self):
+        """" Subtract two number test """
+        res = calc.subtract(10, 6)
+        self.assertEqual(res, 4)
